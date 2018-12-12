f5ca1418b0c434b5daaa3107c6fa4ad4e0224000
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2018-12-12 17:03:11 +0100

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
+Movie.destroy_all
 host = "https://s3.eu-central-1.amazonaws.com/moviedatabase1/"
 
 names = %w(Jupiter INCEPTION Avengers WonderWoman INTERSTELLAR ARRIVAL Rouge\ ONE The\ Martian Passengers John\ Wick2 The\ Wolf\ of\ Wall\ Street Django)
-images = %w(jupiter.jpg inception.jpeg avengers.jpeg wonderwoman.jpeg interstellar.jpeg arrival.jpeg rougueone.png themartian.jpeg passengers.jpeg johnwick2.jpeg thewolf.jpg django.jpeg)
+images = %w(jupiter.jpg inception2.jpeg avengers.jpeg wonderwoman.jpeg interstellar.jpeg arrival.jpeg rougueone.png themartian.jpeg passengers.jpeg johnwick2.jpeg thewolf.jpg django.jpeg)
 
 names.size.times {|i| Movie.create(image: "#{host}#{images[i]}", name: names[i]) } 
