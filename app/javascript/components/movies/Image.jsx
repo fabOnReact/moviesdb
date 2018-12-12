1750300a9dc631314ef248ee72b4846e3394dbe1
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2018-12-12 16:52:47 +0100

    options button alternating images

--- app/javascript/components/movies/Image.jsx
+++ app/javascript/components/movies/Image.jsx
@@ -2,8 +2,22 @@ import React from "react"
 import PropTypes from "prop-types"
 
 export class Image extends React.Component {
+  constructor(props) {
+    super(props);
+    this.images = this.props.images
+    this.state = { image: this.images.not_selected}
+    this.selected = false
+    this.changeState = this.changeState.bind(this)
+  }
+
+  changeState() {
+    const new_state = this.selected ? this.images.not_selected : this.images.selected
+    this.selected = !this.selected
+    this.setState({image: new_state});
+  }
+
   render () {
-    return <img src={this.props.image}/>;
+    return <img onClick={this.changeState} src={this.state.image}/>;
   }
 }
 
--- app/javascript/components/movies/Options.js
+++ app/javascript/components/movies/Options.js
@@ -3,22 +3,30 @@ import PropTypes from "prop-types"
 import { Image } from "./Image"
 import { Icons } from "../../variables/Icons"
 import Add from 'images/add.png'
+import Remove from 'images/remove.png'
 import Play from 'images/play-button.png'
 import Download from 'images/down-arrow.png'
+import Finished from 'images/finished.png'
 import Like from 'images/like.png'
+import Unlike from 'images/unlike.png'
 
 export class Options extends React.Component {
+
   render () {
+    const add = {not_selected: Remove, selected: Add}
+    const play = {not_selected: Play, selected: Play}
+    const download = {not_selected: Download, selected: Finished}
+    const like = {not_selected: Like, selected: Unlike}
     return (
       <React.Fragment>
         <div className="options">
           <div className="actions">
-            <Image src="/download" image={Add} />
-            <Image src="/trailer" image={Play} />
-            <Image src="/wishlist" image={Download} />
+            <Image images={add} />
+            <Image images={play} />
+            <Image images={download} />
           </div>
           <div className="meta">
-            <Image className="metadata" image={Like} />
+            <Image className="metadata" images={like} />
           </div>
         </div>
       </React.Fragment>
