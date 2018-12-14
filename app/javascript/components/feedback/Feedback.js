717df497f73d7738da9ea409f834dabeb6d807ad
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2018-12-14 17:07:31 +0100

    applying stackoverflow solution

--- app/javascript/components/feedback/Feedback.js
+++ app/javascript/components/feedback/Feedback.js
@@ -10,7 +10,6 @@ export class Feedback extends React.Component {
     super(props);
     this.state = { icons: [NoStarIcon, NoStarIcon, NoStarIcon, NoStarIcon, NoStarIcon] };
     this.changeState = this.changeState.bind(this);
-    this.listIcons = [0,1,2,3,4].map((i) => <Star src={this.state.icons[i]} changeState={this.changeState} key={i} id={i+1} />);
   }
 
   changeState(){
@@ -25,13 +24,7 @@ export class Feedback extends React.Component {
       <div className='col-md-2 col-6 playbill container'>
         <p>You can provide us a feedback on this movie or return to the previous page</p>
         <div className="stars">
-          <Star src={this.state.icons[0]} changeState={this.changeState}  id={1} key={0}/> 
-          <Star src={this.state.icons[1]} changeState={this.changeState}  id={2} key={1}/> 
-          <Star src={this.state.icons[2]} changeState={this.changeState}  id={3} key={2}/> 
-          <Star src={this.state.icons[3]} changeState={this.changeState}  id={4} key={3}/> 
-          {/*
-          {this.listIcons}
-          */}
+          { [0,1,2,3,4].map((i) => <Star src={this.state.icons[i]} changeState={this.changeState} key={i} id={i+1} />) }
         </div>
         <button className='btn btn-primary'>back</button>
         {/*<button className='btn btn-warning'>submit</button>*/}
