897efff50c1f1ff1d636baab7f40ceba347129af
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2018-12-15 20:20:58 +0100

    notification test working

---
+++ app/javascript/components/Notification.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NotificationSystem from 'react-notification-system';
+
+export default class Notification extends React.Component {
+  constructor(props){
+    super(props);
+    this.notificationSystem = React.createRef();
+  }
+
+  addNotification = event => {
+    event.preventDefault();
+    const notification = this.notificationSystem.current;
+    notification.addNotification({
+      message: 'Notification message',
+      level: 'success'
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.addNotification}>Add notification</button>
+        <NotificationSystem ref={this.notificationSystem} />
+      </div>
+    );
+  }
+}
+
